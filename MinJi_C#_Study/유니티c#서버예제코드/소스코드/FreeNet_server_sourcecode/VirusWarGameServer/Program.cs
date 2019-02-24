5649cab1b375d02eef04dff20ddac507131a93a3
정민지 <jmj627a@gmail.com>
2019-02-24 15:07:50 +0900

    update

--- MinJi_C#_Study/유니티c#서버예제코드/소스코드/FreeNet_server_sourcecode/VirusWarGameServer/Program.cs
+++ MinJi_C#_Study/유니티c#서버예제코드/소스코드/FreeNet_server_sourcecode/VirusWarGameServer/Program.cs
@@ -36,19 +36,20 @@ namespace VirusWarGameServer
 			Console.ReadKey();
 		}
 
-		/// <summary>
-		/// 클라이언트가 접속 완료 하였을 때 호출됩니다.
-		/// n개의 워커 스레드에서 호출될 수 있으므로 공유 자원 접근시 동기화 처리를 해줘야 합니다.
-		/// </summary>
-		/// <returns></returns>
-		static void on_session_created(CUserToken token)
-		{
-			CGameUser user = new CGameUser(token);
-			lock (userlist)
-			{
-				userlist.Add(user);
-			}
-		}
+        /// <summary>
+        /// 클라이언트가 접속 완료 하였을 때 호출됩니다.
+        /// n개의 워커 스레드에서 호출될 수 있으므로 공유 자원 접근시 동기화 처리를 해줘야 합니다.
+        /// </summary>
+        /// <returns></returns>
+        static void on_session_created(CUserToken token)
+        {
+            CGameUser user = new CGameUser(token);
+            lock (userlist)
+            {
+                userlist.Add(user);
+            }
+        }
+
 
 		public static void remove_user(CGameUser user)
 		{
