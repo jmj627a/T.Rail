551b95d4c42c6ec3f6cb6db76b530ec4d3d724f5
hyewon.park <v_vpollo@naver.com>
2019-03-18 03:01:14 +0900

    190318회의+도트+기획수정+트레인구성

--- T_RAIL/Assets/Move_Actor_F/Player/Player_Actor.cs
+++ T_RAIL/Assets/Move_Actor_F/Player/Player_Actor.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Player_Actor : Move_Actor {
 
 
-    Player_Actor()
+    public Player_Actor()
     {
         base.Actor_Property = (int)Actor.Player; // property에 player 라고 정의
 
+        speed = 10.0f; // speed 는 km/h 로 따지나 
     }
 
-    
+
+
+
+
 }
--- T_RAIL/Assets/Move_Actor_F/Player_Move.cs
+++ T_RAIL/Assets/Move_Actor_F/Player_Move.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 public class Player_Move : MonoBehaviour {
 
 
+    Player_Actor player1 = new Player_Actor();
+
 	// Update is called once per frame
 	void Update () {
 
         // switch- case는 쓰는데
         // 키 동시에 입력되면?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         if (Input.GetKeyDown(KeyCode.A))
         {
             // x = -1
@@ -27,6 +46,21 @@ public class Player_Move : MonoBehaviour {
             // y = +1
         }
 
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            // 공격
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Jump
+        }
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            // 카메라 전환 
+        }
 
     }
 }
