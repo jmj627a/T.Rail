97bcd86905b450ba86157b3cb2347f381ec1eec8
정민지 <jmj627a@gmail.com>
2019-04-05 07:51:40 +0900

    CubeGame 클라이언트와 로비를 붙여봄

---
+++ MinJi_Study/CubeGame/Assets/Scripts/lobbyController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class lobbyController : MonoBehaviourPunCallbacks {
+
+
+    #region Game UI
+    public InputField playerNameInputField;
+
+    
+    void setPlayerRandomName()
+    {
+        playerNameInputField.text = "Player " + Random.Range(1000, 10000);
+    }
+
+    #endregion
+
+
+
+    #region Unity
+
+    private void Awake()
+    {
+        setPlayerRandomName();
+    }
+
+
+    #endregion
+
+
+    #region Photon Callbacks
+
+    public void OnLoginButtonClicked()
+    {
+        string playerName = playerNameInputField.text;
+
+        if (!playerName.Equals(""))
+        {
+            PhotonNetwork.LocalPlayer.NickName = playerName;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        else
+        {
+            Debug.LogError("Player Name is invalid.");
+        }
+
+
+        //플레이어가 ready!를 누르면 이 버튼이 활성화 되고, 이걸 누르면 게임시작
+        //public void OnStartGameButtonClicked()
+        {
+            //PhotonNetwork.CurrentRoom.IsOpen = false;           //더이상 플레이어들이 참여 못하게
+            //PhotonNetwork.CurrentRoom.IsVisible = false;        //더이상 다른 사람에게 로비가 보이지도 않게
+
+            PhotonNetwork.LoadLevel("MainScene"); //겜 시작하면 게임화면 씬 로드
+        }
+    }
+
+    #endregion
+}
---
+++ MinJi_Study/CubeGame/Assets/Scripts/playerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    #region private 
+
+    Transform thisObject;
+
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+
+        thisObject = this.GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        playerKeyInput();
+    }
+
+
+    void playerKeyInput()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            thisObject.position += new Vector3(0, 0, 5) * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            thisObject.position += new Vector3(-5, 0, 0) * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            thisObject.position += new Vector3(0, 0, -5) * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            thisObject.position += new Vector3(5, 0, 0) * Time.deltaTime;
+        }
+        else if (Input.GetKey (KeyCode.Space))
+        {
+            thisObject.position += new Vector3(0, 5, 0) * Time.deltaTime;
+        }
+    }
+}
