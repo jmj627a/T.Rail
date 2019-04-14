63696ed5525f2ff4f0d6b30b6a1e1bc9d6f1b956
Park Hye Won <v_vpollo@naver.com>
2019-04-15 02:00:41 +0900

    천장으로 올라가기 추가중

--- T_RAIL/Assets/_Resource/Scripts/Train/Train_Object.cs
+++ T_RAIL/Assets/_Resource/Scripts/Train/Train_Object.cs
@@ -16,6 +16,11 @@ public class Train_Object : MonoBehaviour{
     bool Position_Set_Go; // 포지션을 세팅하면서 달려와서 붙는거
     Vector3 Position_Set_Destination;
 
+
+    int passenger; // 이 기차에 승객이 몇명있는지
+    int box;  // 이 기차에 박스 몇개있는지
+
+
     float Coroutine_calltime; // 코루틴 안끄곸ㅋㅋㅋㅋ 그 안에 호출할 상황이면 0.01
                               // 호출안할 상황이면 0.5
 
