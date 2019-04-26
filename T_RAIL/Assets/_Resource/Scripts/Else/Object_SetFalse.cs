7f3e77f285592596d9e2cc18f55183bf7dbd9912
Park Hye Won <v_vpollo@naver.com>
2019-04-27 04:13:34 +0900

    사다리 올라가는 거 수정 + 2층에서 마우스로 카메라 이동 수정 + particle package(casual) 추가

---
+++ T_RAIL/Assets/_Resource/Scripts/Else/Object_SetFalse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_SetFalse : MonoBehaviour {
+
+    public float set_time;
+
+    private void OnEnable()
+    {
+        Invoke("active_off", set_time);
+    }
+    void active_off()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+}
